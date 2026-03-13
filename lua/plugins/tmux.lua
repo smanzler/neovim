2c2da71b0e6bc19df4d52a3953be32abd3bb8069
Simon Manzler <simanzler@gmail.com>
2026-03-13 12:49:08 -0400

    added missing untracked file

---
+++ lua/plugins/tmux.lua
@@ -0,0 +1,10 @@
+return {
+	'christoomey/vim-tmux-navigator',
+	lazy = false,
+	keys = {
+		'<C-j>', '<cmd> TmuxNavigateLeft<cr>', desc='window left',	
+		'<C-j>', '<cmd> TmuxNavigateRight<cr>', desc='window right',	
+		'<C-j>', '<cmd> TmuxNavigateUp<cr>', desc='window up',	
+		'<C-j>', '<cmd> TmuxNavigateDown<cr>', desc='window down',	
+	},
+}
