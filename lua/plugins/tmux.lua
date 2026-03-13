2ac20d0f19fa5b0c9cbead61f59295150392afb4
Simon Manzler <simanzler@gmail.com>
2026-03-13 14:11:38 -0400

    added mini

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -2,9 +2,9 @@ return {
 	'christoomey/vim-tmux-navigator',
 	lazy = false,
 	keys = {
-		'<C-j>', '<cmd> TmuxNavigateLeft<cr>', desc='window left',	
-		'<C-j>', '<cmd> TmuxNavigateRight<cr>', desc='window right',	
-		'<C-j>', '<cmd> TmuxNavigateUp<cr>', desc='window up',	
-		'<C-j>', '<cmd> TmuxNavigateDown<cr>', desc='window down',	
-	},
+		{ '<C-j>', '<cmd> TmuxNavigateLeft<cr>', desc='window left' },
+		{ '<C-j>', '<cmd> TmuxNavigateRight<cr>', desc='window right' },
+		{ '<C-j>', '<cmd> TmuxNavigateUp<cr>', desc='window up' },
+		{ '<C-j>', '<cmd> TmuxNavigateDown<cr>', desc='window down' },
+	}
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,12 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  main = 'nvim-treesitter.config',
+  opts = {
+    ensure_installed = {
+      "lua", "vim", "vimdoc", "javascript", "typescript", "tsx", "html", "css"
+    },
+    auto_install = true
+  }
 }
